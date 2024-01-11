2e1a92eae042dd7f6fccd51df8fdec042eadac14
serigano <k.serigano.cingroup2012@gmail.com>
2024-01-11 18:00:41 +0900

    first commit

---
+++ lesson3.php
@@ -0,0 +1,24 @@
+﻿<?php
+$pro = array(
+    'id' => 'ID',
+    'name' => '山田',
+    'age' => 20,
+    'gender' => '女性',
+);
+
+echo $pro['name'];
+echo '<br>';
+echo $pro['age'];
+echo $pro['gender'];
+
+
+
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
